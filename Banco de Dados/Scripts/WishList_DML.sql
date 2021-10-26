cbbe584ee941dc75434e19b12313408ba804c94f
Wesley <wmixgamer@gmail.com>
2021-10-26 09:29:46 -0300

    Scripts do Banco de Dados adicionados

---
+++ Banco de Dados/Scripts/WishList_DML.sql
@@ -0,0 +1,7 @@
+USE WISHLIST_G4
+
+INSERT INTO USUARIOS (nome, email, senha)
+VALUES ('Vinicius', 'vinicius@email.com', 'vini123'), ('Wesley', 'wesley@email.com', 'wesley1980')
+
+INSERT INTO DESEJOS (idUsuario, descricao)
+VALUES (1, 'Descrição para teste'), (2, 'Qualquer coisa para teste')
